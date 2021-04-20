cbfe68039e052ebf3855301a30054df44f4d8e8f
Zach Salem <zsalem2023@gmail.com>
2021-04-19 22:11:37 -0400

    home view and controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,18 @@
 # This controller generates the homepage using a GET request
 class HomeController < ApplicationController
   # The GET request must render the projects, team that the project was assigned to, and the survey for that project.
-  def get_info
-    render json: Project.all
-    project_team = TeamProject.all
-    surveys = Survey.all
+  def index
+    if user_signed_in?
+      @rows = current_user.student.team_projects.collect{ |team_project|
+        {
+          class_name: "Class Name Here",
+          team_name: team_project.team.name,
+          project_name: team_project.project.name,
+          action_link: if team_project.comments_for_team(current_user.student).empty? then "/survey/complete?id="+team_project.id.to_s else "/survey/view/id="+team_project.id.to_s end,
+          action_text: if team_project.comments_for_team(current_user.student).empty? then "Complete" else "View" end,
+          due: team_project.project.due.in_time_zone('Eastern Time (US & Canada)').strftime('%Y-%m-%d %I:%M:%S %p')
+        }
+      }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'welcome#index'
+  root to: 'home#index'
 
   get '/student/index', to: 'student#index'
   post '/student/index', to: 'student#create'
