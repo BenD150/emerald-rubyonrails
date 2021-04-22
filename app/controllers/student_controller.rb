52c017fcc9124c7fc81b10f941d1816442cbcf30
Zach Salem <zsalem2023@gmail.com>
2021-04-21 23:43:25 -0400

    used layout for all views added option to add instructors for a course

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -25,12 +25,27 @@ class StudentController < ApplicationController
       name = student_info[0].split
       first = name[0]
       last = name[1]
+      is_instructor = student_info.length > 2
 
       student = Student.where(email: email).first
+      instructor = Instructor.where(email: email).first
+      user = User.where(email: email).first
+
+      # if instructor doesn't exist, create
+      if is_instructor && !instructor
+        if user
+          # if corresponding user exists, reference this new instructor
+          instructor = Instructor.create(email: email, user: user)
+        else
+          # otherwise, assume we will add the user reference later when the user is created
+          instructor = Student.create(email: email)
+        end
+
+        # match instructor with current course
+        CourseInstructor.create(course: $selected_course, instructor: instructor)
 
       # if student doesn't exist, create
-      unless student
-        user = User.where(email: email)
+      elsif !student
         if user
           # if corresponding user exists, reference this new student
           student = Student.create(first: first, last: last, email: email, user: user)
@@ -38,10 +53,10 @@ class StudentController < ApplicationController
           # otherwise, assume we will add the user reference later when the user is created
           student = Student.create(first: first, last: last, email: email)
         end
-      end
 
-      # match student with current course
-      CourseStudent.create(course: $selected_course, student: student)
+        # match student with current course
+        CourseStudent.create(course: $selected_course, student: student)
+      end  
     end
 
     render json: { success: true }
