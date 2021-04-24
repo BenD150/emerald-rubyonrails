b103954d5ff766582097500daead532f2b867782
Zach Salem <zsalem2023@gmail.com>
2021-04-23 20:39:38 -0400

    fix datetime compare

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,8 +17,8 @@ class HomeController < ApplicationController
           class_name: team_project.team.course.name,
           team_name: team_project.team.name,
           project_name: team_project.project.name,
-          action_link: if team_project.comments_for_team(current_user.student).empty? then "/survey/complete?id="+team_project.id.to_s elsif team_project.project.due > DateTime.now() then "/survey/view?id="+team_project.id.to_s else nil end,
-          action_text: if team_project.comments_for_team(current_user.student).empty? then "Complete" elsif team_project.project.due > DateTime.now() then "View" else "Awaiting Results" end,
+          action_link: if team_project.comments_for_team(current_user.student).empty? then "/survey/complete?id="+team_project.id.to_s elsif team_project.project.due.before? 1.second.ago then "/survey/view?id="+team_project.id.to_s else nil end,
+          action_text: if team_project.comments_for_team(current_user.student).empty? then "Complete" elsif team_project.project.due.before? 1.second.ago then "View" else "Awaiting Results" end,
           due: team_project.project.due.in_time_zone('Eastern Time (US & Canada)').strftime('%Y-%m-%d %I:%M:%S %p')
         }
       }
